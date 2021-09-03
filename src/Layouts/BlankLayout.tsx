9d96be6be371f42bf4ad50405f1cfbe01f551651
Tran Van Tuan Trung <57562063+tranvantuantrung@users.noreply.github.com>
2021-09-03 16:24:27 +0700

    Delete src/Layouts directory

--- src/Layouts/BlankLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { ILayout } from './interface'
-
-const BlankLayout: React.FC<ILayout> = ({ children }) => {
-  return <div>{children}</div>
-}
-
-export default BlankLayout
--- src/Layouts/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as MainLayout } from './MainLayout';
-export { default as BlankLayout } from './BlankLayout';
