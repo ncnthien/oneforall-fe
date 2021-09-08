8b76c4fbde42db7eab100140ddb7bc23f572aa7c
tuantrung <tuantrung.dev@gmail.com>
2021-09-09 00:15:55 +0700

    add facebook svg and google svg

---
+++ src/assets/images/svgs/FacebookBtnIcon.tsx
@@ -0,0 +1,21 @@
+const FacebookBtnIcon: React.FC = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 26.78 26.78'
+    >
+      <path
+        d='M23.43 0H3.35A3.35 3.35 0 000 3.35v20.08a3.35 3.35 0 003.35 3.35h20.08a3.35 3.35 0 003.35-3.35V3.35A3.35 3.35 0 0023.43 0z'
+        fill='#395398'
+      ></path>
+      <path
+        d='M18.38 26.78V16.5h3.44l.52-4h-4V9.93c0-1.16.32-1.95 2-1.95h2.12V4.4a29.07 29.07 0 00-3.09-.16c-3.06 0-5.16 1.87-5.16 5.3v3h-3.44v4h3.46v10.24z'
+        fill='#fff'
+      ></path>
+    </svg>
+  )
+}
+
+export default FacebookBtnIcon
