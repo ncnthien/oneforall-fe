0a3413a03503dc6dafbd98bb80c3b6fdbb8a19a0
tuantrung <tuantrung.dev@gmail.com>
2021-09-11 16:13:37 +0700

    rename for breadcrumb

---
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import './Breadcrumb.scss'
+import { IBreadcrumb } from './interface'
+import logo from 'assets/images/logo.png'
+
+const Breadcrumb: React.FC<IBreadcrumb> = ({ url, name }) => {
+  return (
+    <div className='breadcrumb d-flex align-items-center'>
+      <Link to='/' className='breadcrumb__logo'>
+        <img src={logo} alt='oneforall' className='breadcrumb__img' />
+      </Link>
+      <span className='breadcrumb__slash d-block mx-1 font-bold size-14'>
+        /
+      </span>
+      <Link
+        to={url}
+        className='breadcrumb__link font-bold size-14 color-black text-decoration-none'
+      >
+        {name}
+      </Link>
+    </div>
+  )
+}
+
+export default Breadcrumb
